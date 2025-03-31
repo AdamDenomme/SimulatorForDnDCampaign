2c8d6ffd7176a6b6088a2342da5d0688e491b973
Adam_Denomme <sorodemoro@gmail.com>
2025-03-31 04:29:33 -0700

    Converted Drag and Drop to C++
    - All Drag and Drop for DnD Units is functional in C++
    - Fixed a bug where Game would crash if Raycast did not hit a Valid Actor
    - Revised code to only drag pawns and not actors
    - Polished UX a bit to where the Object does not drop prematurely if User moves mouse faster than Object is moving.

--- Source/DnDSimulator/Private/DnDPlayerController.cpp
+++ Source/DnDSimulator/Private/DnDPlayerController.cpp
@@ -2,6 +2,9 @@
 
 
 #include "DnDPlayerController.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "../DnDSimulator.h"
 
 void ADnDPlayerController::BeginPlay()
 {
@@ -15,17 +18,131 @@ void ADnDPlayerController::BeginPlay()
     bEnableMouseOverEvents = true;
 }
 
+void ADnDPlayerController::SetupInputComponent()
+{
+    Super::SetupInputComponent();
+
+    //Left Mouse Button: Drag and Drop Units
+    InputComponent->BindAction("DragUnit", IE_Pressed, this, &ADnDPlayerController::SetToIsBeingDragged);
+    InputComponent->BindAction("DragUnit", IE_Released, this, &ADnDPlayerController::SetToIsNotBeingDragged);
+}
+
 void ADnDPlayerController::Tick(float DeltaSeconds)
 {
     Super::Tick(DeltaSeconds);
     SetInputModeGameOnly(true);
+
+    // Set Up Gate Logic: 
+    // - Enter Every Frame
+    // - Open Gate on Left Mouse Click
+    // - Close Gate on Left Mouse Release
+
+    if (IsBeingDragged)
+    {
+        DragObject();
+        UE_LOG(LogTemp, Warning, TEXT("Input Drag: Object Picked Up!"));
+
+    }
+    else
+    {
+        DropObject();
+        UE_LOG(LogTemp, Warning, TEXT("Input Drag: Object Dropped!"));
+    }
 }
 
-void ADnDPlayerController::SetInputModeGameOnly(
-    bool InConsumeCaptureMouseDown)
+void ADnDPlayerController::SetInputModeGameOnly(bool InConsumeCaptureMouseDown)
 {
     FInputModeGameOnly InputMode;
     InputMode.SetConsumeCaptureMouseDown(
         InConsumeCaptureMouseDown);
     SetInputMode(InputMode);
+}
+
+void ADnDPlayerController::DragObject()
+{
+    // Get Player Controller
+    ADnDPlayerController* PlayerController = Cast<ADnDPlayerController>(GetWorld()->GetFirstPlayerController());
+
+    if (PlayerController)
+    {
+        // Get Camera from World
+        APlayerCameraManager* CameraManager = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0);
+        FVector CameraLocation = CameraManager->GetCameraLocation();
+
+        // Variables for MouseLocation
+        FVector WorldLocation;
+        FVector WorldDirection;
+
+        // Convert Mouse Location to a 3D ray where...
+        // WorldLocation: Where the Raycast starts (Cursor is Above Object)
+        // WorldDirection: Direction the Ray is casted
+        PlayerController->DeprojectMousePositionToWorld(WorldLocation, WorldDirection);
+        
+        // Cast Ray 10000 Units from Raycast Position
+        FVector MouseLocation = WorldLocation + (WorldDirection * 10000);
+
+        // Define Unit Location
+        FVector UnitLocation = HitActor->GetActorLocation();
+
+        // Define Point on 3D plane where intersection will happen
+        FVector PlaneNormalization = FVector(0, 0, 1.0);
+
+        // Define new location the Unit will be moved to
+        FVector NewUnitLocation; 
+
+        // Honestly hes just a vibe dawg...
+        float T = 0; 
+
+        // This is a bool...
+        bool NewUnitLocationExistence = UKismetMathLibrary::LinePlaneIntersection_OriginNormal(CameraLocation, MouseLocation, UnitLocation, PlaneNormalization, T, NewUnitLocation);
+
+        // If true, set UnitLocation to new UnitLocation
+        if (NewUnitLocationExistence)
+        {
+            HitActor->SetActorLocation(NewUnitLocation);
+        }
+        
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Input Drag: No Player Controller Found. Can not Drag Unit!"));
+    }
+}
+
+void ADnDPlayerController::DropObject()
+{
+    if (HitActor)
+    {
+        HitActor = nullptr;
+    }
+}
+
+void ADnDPlayerController::SetToIsBeingDragged()
+{
+    // Raycast Under the Cursor and Get the Hit Result
+    FHitResult HitResult;
+    bool BlockingHit = GetHitResultUnderCursorByChannel(UEngineTypes::ConvertToTraceType(ECC_Visibility), true, HitResult);
+    if (BlockingHit)
+    {
+        // Valid to Continue if it Hits a Pawn (NOT AN ACTOR)
+        APawn* HitPawn = Cast<APawn>(HitResult.GetActor());
+
+        if (IsValid(HitPawn))
+        {
+            HitActor = HitPawn;
+            IsBeingDragged = true;
+            UE_LOG(LogTemp, Log, TEXT("Hit Actor: %s"), *HitActor->GetName());
+        }
+    }
+    else
+    {
+        HitActor = nullptr;
+        UE_LOG(LogTemp, Warning, TEXT("Input Drag: Not Valid Hit!"));
+        return;
+    }
+}
+
+void ADnDPlayerController::SetToIsNotBeingDragged()
+{
+    IsBeingDragged = false;
 }
--- Source/DnDSimulator/Public/DnDPlayerController.h
+++ Source/DnDSimulator/Public/DnDPlayerController.h
@@ -14,15 +14,35 @@ class DNDSIMULATOR_API ADnDPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 public:
-
 	virtual void BeginPlay() override;
-	virtual void Tick(float DeltaSeconds) override;
-protected:
 
+	//Used to Update Location of Dragged Object/Fix Double Click
+	virtual void Tick(float DeltaSeconds) override;
+
+	AActor* HitActor;
+
+protected:
 	// Override SetupInputComponent to bind inputs
 	virtual void SetupInputComponent() override;
-
 	void SetInputModeGameOnly(bool InConsumeCaptureMouseDown);
+
+private:
+
+	// Drag and Drop Units Functionality
+	UFUNCTION()
+	void DragObject();
+
+	UFUNCTION()
+	void DropObject();
+
+	// Drag and Drop Units Gate Logic
+	UFUNCTION()
+	void SetToIsBeingDragged();
+
+	UFUNCTION()
+	void SetToIsNotBeingDragged();
+
+	bool IsBeingDragged;
 
 	
 };
